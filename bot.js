a365e7a4567ed6b6a6aaf2a180ba6cf1a2a92ae5
jonathan sundberg <guitaraddict95@hotmail.com>
2018-07-09 02:38:00 +0200

    rah!

--- bot.js
+++ bot.js
@@ -2,7 +2,8 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./auth.json");
 const fs = require("fs");
-const token = process.env.config;
+
+
 client.on("ready", () => {
   console.log("I am ready!");
 });
@@ -110,4 +111,4 @@ client.on("message", (message) => {
 
 });
 
-client.login(process.env.NDY1Mjc2OTE5NzcyODcyNzU0.DiLKqw.WCRtg7mV-7QcAG3TnBm6DhP5_go).catch(err => console.log(err));
+client.login(process.env.token).catch(err => console.log(err));
